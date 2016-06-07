38c194e3c8127b000e3dd105816fdf04f3d350d0
rsharm15 <rahul.sharma@coveros.com>
2016-06-07 14:56:59 -0400

    initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "rahulsharma"
+client_key               "#{current_dir}/rahulsharma.pem"
+validation_client_name   "coveros-validator"
+validation_key           "#{current_dir}/coveros-validator.pem"
+chef_server_url          "https://ec2-52-87-191-238.compute-1.amazonaws.com/organizations/coveros"
+cookbook_path            ["#{current_dir}/../cookbooks"]
